ca2fcd8fafee3ef26aaf6bce6e310c9bdd368528
恶童历险记 <etonglixianji@gmail.com>
2014-07-04 01:54:03 +0800

    f4

--- FindViewById2.py
+++ FindViewById2.py
@@ -1,39 +1,69 @@
 #encoding=utf-8
+import sys
+import os
 from xml.etree import ElementTree as ET
+def classdef(str,filename):
+	fileS = filename.split("\\")
+	fileName = fileS[len(fileS)-1].split("_")[0].capitalize() + "Activity"
+	classdef = "public class " + fileName +" extends Activity {\n" + str + "\n}"
+	return classdef
 
-def modify(temp,defineType):
+def declare(temp,defineType):
 	item = temp.split('/')[1]
-	tempR = defineType + " " +item + " = (" + defineType +")findViewById(R.id."+ item + ");"
+	tempR = "\tprivate " + defineType + " " + item + ";"
 	return tempR
 
-#读取文件的路径
-read_file = 'D:/work/github/workspace-py/PyForAndroid/xml_for_test/activity_main.xml'
-#写入的文件路径
-file_path = open("D://work//github//workspace-py//PyForAndroid//result.java","w")
+def implementString(temp,defineType):
+	item = temp.split('/')[1]
+	tempR = "\t\t" + item + " = (" + defineType +")findViewById(R.id."+ item + ");"
+	return tempR
+
+def findAllView(str):
+	findAllViewStr = "\tprivate void findAllView(){" + "\n" + str + "\n" + "\t}"
+	return findAllViewStr
+
+def make_file(fileName):
+	#结果列表
+	resultDef = []
+	resultImple = []
+
+	per=ET.parse(fileName)
+
+	for typeT in typelist:
+		p=per.findall("./"+typeT)
+		for oneper in p:  
+			temp = oneper.get(prefix+'id')
+			tempD = declare(temp,typeT)
+			tempI = implementString(temp,typeT)
+			resultDef.append(str(tempD))
+			resultImple.append(str(tempI))
+
+	finalStringI = '\n'.join(resultImple)
+	finalStringD = '\n'.join(resultDef)
+
+	string1 = findAllView(finalStringI)
+	string2 = finalStringD + "\n\n"+ string1
+	string3 = classdef(string2,fileName)
+	return string3
+
+#输入的目录路径
+dir_path = sys.argv[1]
+
+#输出指定的文件夹目录
+dir_write = "D://work//github//workspace-py//PyForAndroid"
+
 #固定前缀
 prefix = "{http://schemas.android.com/apk/res/android}"
 
-#结果列表
-result = []
-
 #需要找的标签
-typelist = ['TextView' , 'Button']
+typelist = ['TextView' , 'Button' , 'ImageView']
 
-per=ET.parse(read_file)
-
-for typeT in typelist:
-	p=per.findall("./"+typeT)
-	for oneper in p:  
-		temp = oneper.get(prefix+'id')
-		temp = modify(temp,typeT)
-		print ("temp is ", temp)
-		result.append(str(temp))
-
-print (result)
-finalString = '\n'.join(result)
-file_path.write(finalString)
-file_path.close()
-
-
-    
-    
+#读取文件的路径
+for root, dirs, files in os.walk(dir_path):
+    for f in files:
+    	fileFullName = os.path.join(root, f)
+    	print (fileFullName)
+    	str = make_file(fileFullName)
+    	file_path_write = open(fileFullName.split(".")[0] + ".java","w")
+    	file_path_write.write(str)
+    	file_path_write.close()
